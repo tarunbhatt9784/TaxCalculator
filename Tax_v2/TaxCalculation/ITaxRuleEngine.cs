29a44a6bdb7ddf822187c5d641acbce47399ead7
Ruhee Hayat <ruhee.hayat@hotmail.com>
2022-09-30 08:27:22 +0530

    .net

---
+++ Tax_v2/TaxCalculation/ITaxRuleEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxCalculator.TaxCalculation
+{
+    public interface ITaxRuleEngine
+    {
+        TaxPayer RunTaxEngine(TaxPayer taxPayer);
+    }
+}
